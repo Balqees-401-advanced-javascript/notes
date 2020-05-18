b2d6c3aa8ee8443c24c531a853824f5907686284
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-05-18 23:06:02 +0300

    result show

--- notes.js
+++ notes.js
@@ -1,6 +1,10 @@
 'use strict'
 
-const Input = require('./lib/input');
-const Notes = require('./lib/notes');
+const Input = require('./lib/input.js');
+const Notes = require('./lib/notes.js');
 
-const 
+const validInputNotev= new Input(); 
+// const noteNew = new Notes;
+
+Notes.execute(validInputNotev);
+
