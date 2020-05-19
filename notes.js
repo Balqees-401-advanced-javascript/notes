5058145e8383ef68f0064954cdbe6d04b4594a23
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-05-20 01:27:00 +0300

    not solve

--- notes.js
+++ notes.js
@@ -1,7 +1,18 @@
 'use strict'
 
+const mongoose = require('mongoose');
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
+
+//connecting the mongoose
+const DB_URI = 'mongodb://localhost:27017/save';
+
+// for error things
+mongoose.connect(DB_URI,{
+    userNewUrlParse: true, 
+    useUnifiedTopology: true
+});
+
 
 const validInputNotev= new Input(); 
 const note = new Notes();
