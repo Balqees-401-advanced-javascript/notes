f58ee8f644961055e7a8094f3d55999bacdfb59b
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-05-29 19:00:33 +0300

    try

--- notes.js
+++ notes.js
@@ -1,24 +1,8 @@
 'use strict';
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
-
-// bring Scheme
-
-
-//connecting the mongoose "connect to DB" //define URL
-const DB_URI = 'mongodb://localhost:27017/save';// this will be in .env file in future and call it py process.env.DB_URI
-// (/save) is the DB name if it's not exiest it wil creat it for me 
-
-
-// for error things
-mongoose.connect(DB_URI,{
-  useNewUrlParser: true, useUnifiedTopology: true,
-});
-
-//async await : callbacks to wait 
-//await schema.save(); // save to DB
 
 
 const validInputNotev= new Input(); 
@@ -26,4 +10,26 @@ const note = new Notes();
 
 
 note.execute(validInputNotev);
+
+
+// bring Scheme
+
+
+//connecting the mongoose "connect to DB" //define URL
+// const DB_URI = 'mongodb://localhost:27017/save';// this will be in .env file in future and call it py process.env.DB_URI
+// (/save) is the DB name if it's not exiest it wil creat it for me 
+
+
+// for error things
+
+// mongoose.connect(DB_URI,{
+//   useNewUrlParser: true, useUnifiedTopology: true,
+// })
+//   .catch(err => console.log( err ));
+
+//async await : callbacks to wait 
+//await schema.save(); // save to DB
+
+
+
 
