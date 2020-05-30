ed7db130b67de25ed1ae67fe8904f6ce849789ea
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-05-30 16:55:38 +0300

    done

--- __tests__/input.test.js
+++ __tests__/input.test.js
@@ -1,21 +1,51 @@
-'use strict'
-
+/* eslint-disable indent */
+'use strict';
+require('@code-fellows/supergoose');
 const Input = require('../lib/input.js');
+const minimist = require('minimist');
+jest.spyOn(console , 'log');
+jest.mock('minimist');
+
+minimist.mockImplementationOnce(() => ({ 
+  }));
 
 describe('Input Module', ()=> {
-
-    it('validation()  there is no method or invalid', ()=> {
+  it ('validation()  there is no method or invalid', ()=> {
         let validMethod = new Input();
-        expect(validMethod.validation('wrongMethod')).toEqual('wrong input');
+        expect(console.log).toHaveBeenCalled();
+  });
 
-    });
+  minimist.mockImplementationOnce(() => ({ 
+    add: 'this is my first test',
+  }));
 
-    it('validation() validating and using the proper methods', ()=> {
-        let validMethod = new Input();
-        expect(validMethod.validation('--add')).toEqual('add');
-        expect(validMethod.validation('-a')).toEqual('add');
-        // expect(validMethod.validation('-l')).toEqual('add');
-       
-    });
+  it ('add method ' , ()=> {
+    expect(new Input()).toMatchObject({action:'add' , payload:'this is my first test' , category:'general'});
+  });
 
+  minimist.mockImplementationOnce(() => ({
+      delete:'5ed185e8988aa9057074f7c4',
+  }));
+
+  it('delete method', () =>{
+      expect(new Input()).toMatchObject({action:'delete' , id:'5ed185e8988aa9057074f7c4' });
+  });
+
+  minimist.mockImplementationOnce(() => ({
+    update:'5ed185e8988aa9057074f7c4',
+    _:['new'],
+}));
+
+it('update method', () =>{
+    expect(new Input()).toMatchObject({action:'update' , id:'5ed185e8988aa9057074f7c4'});
 });
+
+minimist.mockImplementationOnce(() => ({
+  list:'words',
+}));
+
+it('list method', () =>{
+    expect(new Input()).toMatchObject({action:'list' , list:'words'});
+});
+
+    });
--- notes.js
+++ notes.js
@@ -1,31 +1,29 @@
 'use strict';
 
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
-
-
-const validInputNotev= new Input(); 
-const note = new Notes();
-
-
-note.execute(validInputNotev);
-
-
 // bring Scheme
 
-
 //connecting the mongoose "connect to DB" //define URL
-// const DB_URI = 'mongodb://localhost:27017/save';// this will be in .env file in future and call it py process.env.DB_URI
+const DB_URI = 'mongodb://localhost:27017/save';// this will be in .env file in future and call it py process.env.DB_URI
+// const DB_URI = 'mongodb+srv://balqeesqasem:0000@cluster0-5fmxj.mongodb.net/test?retryWrites=true&w=majority';
 // (/save) is the DB name if it's not exiest it wil creat it for me 
 
 
 // for error things
 
-// mongoose.connect(DB_URI,{
-//   useNewUrlParser: true, useUnifiedTopology: true,
-// })
-//   .catch(err => console.log( err ));
+mongoose.connect(DB_URI,{
+  useNewUrlParser: true, useUnifiedTopology: true,
+})
+  .catch(err => console.log( err ));
+
+
+const validInputNotev= new Input(); 
+const note = new Notes();
+
+note.execute(validInputNotev)
+  .then(mongoose.disconnect);
 
 //async await : callbacks to wait 
 //await schema.save(); // save to DB
